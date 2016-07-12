45bad93eccd178c25557c1d35177616bccbc0a48
Sandeep Sidhu <ssa52@ovp.bskyb.com>
2016-07-12 12:39:52 +0100

    Adding dhcprelay resource files.

---
+++ api/dhcprelay/dhcprelay_types.go
@@ -0,0 +1,18 @@
+package dhcprelay
+
+import "encoding/xml"
+
+type DhcpRelay struct {
+	RelayServer RelayServer  `xml:"relayServer"`
+	RelayAgents []RelayAgent `xml:"relayAgents>relayAgent"`
+}
+
+type RelayServer struct {
+	IpAddress string `xml:"ipAddress"`
+}
+
+type RelayAgent struct {
+	XMLName   xml.Name `xml:"relayAgent"`
+	VnicIndex string   `xml:"vnicIndex"`
+	GiAddress string   `xml:"giAddress"`
+}
---
+++ client_dhcprelay_test.go
@@ -0,0 +1,26 @@
+package client
+
+import (
+	"github.com/sky-uk/paas-gonsx/api/dhcprelay"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"testing"
+)
+
+func TestClientGetAllDhcpRelays(t *testing.T) {
+	xmlContent := `<?xml version="1.0" encoding="UTF-8"?>
+<relay><relayServer><ipAddress>10.152.160.10</ipAddress></relayServer><relayAgents><relayAgent><vnicIndex>17</vnicIndex><giAddress>10.152.164.1</giAddress></relayAgent><relayAgent><vnicIndex>16</vnicIndex><giAddress>10.152.165.1</giAddress></relayAgent></relayAgents></relay>`
+	setup(http.StatusOK, xmlContent)
+	defer server.Close()
+
+	api := dhcprelay.NewGetAll("edge-50")
+	nsxClient.Do(api)
+
+	assert.Equal(t, "10.152.160.10", api.GetResponse().RelayServer.IpAddress)
+	assert.Len(t, api.GetResponse().RelayAgents, 2)
+	assert.Equal(t, "17", api.GetResponse().RelayAgents[0].VnicIndex)
+	assert.Equal(t, "10.152.164.1", api.GetResponse().RelayAgents[0].GiAddress)
+	assert.Equal(t, "16", api.GetResponse().RelayAgents[1].VnicIndex)
+	assert.Equal(t, "10.152.165.1", api.GetResponse().RelayAgents[1].GiAddress)
+
+}
