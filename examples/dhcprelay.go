6b3337ade7decd8daed96495e7495e7c11ec072a
Sandeep Sidhu <sandeep.pal.sidhu@gmail.com>
2016-07-13 10:17:15 +0100

    updating example files

--- examples/dhcprelay.go
+++ examples/dhcprelay.go
@@ -9,25 +9,29 @@ import (
 
 func main() {
 	if len(os.Args) != 4 {
-		fmt.Printf("syntax error\nUsages: %s [NSX Manager Address] [Username] [Password] [INput file]\n\n", os.Args[0])
+		fmt.Printf("syntax error\nUsages: %s [https://nsxmanager_address] [username] [password]\n\n", os.Args[0])
 		os.Exit(1)
 	}
 	nsxManager := os.Args[1]
 	nsxUser := os.Args[2]
 	nsxPassword := os.Args[3]
 
+	//
 	// Create NSXClient object.
-	nsxclient := gonsx.NewNSXClient("https://"+nsxManager, nsxUser, nsxPassword, true, true)
+	//
+	nsxclient := gonsx.NewNSXClient(nsxManager, nsxUser, nsxPassword, true, true)
 
-	// Create DHCPRelay API call reference.
-	getAllDhcpRelay := dhcprelay.NewGetAll("edge-50")
-
-	// Actually making the api call with nsxclient.
-	err := nsxclient.Do(getAllDhcpRelay)
+	//
+	// Get All DHCP Relay agents.
+	//
+	api := dhcprelay.NewGetAll("edge-50")
+	// make the api call with nsxclient
+	err := nsxclient.Do(api)
+	// check if we err otherwise read response.
 	if err != nil {
 		fmt.Println("Error:", err)
 	} else {
-		fmt.Println(getAllDhcpRelay.GetResponse())
+		fmt.Println(api.GetResponse())
 	}
 
 }
