3fc1a4bcce8258e669f03ab5f36a9c300a56b8ee
rle27 <rui.leitemayermoreira@sky.uk>
2017-04-27 17:17:02 +0100

    added new Revision type

--- api/securitytag/update_securitytag.go
+++ api/securitytag/update_securitytag.go
@@ -3,6 +3,8 @@ package securitytag
 import (
 	"github.com/sky-uk/gonsx/api"
 	"net/http"
+	"fmt"
+	"strconv"
 )
 
 //UpdateSecurityTagAPI - struct
@@ -13,6 +15,11 @@ type UpdateSecurityTagAPI struct {
 //NewUpdate - Generates a new UpdateSecurityTagAPI object.
 func NewUpdate(securityTagID string, securityTagPayload *SecurityTag) *UpdateSecurityTagAPI {
 	this := new(UpdateSecurityTagAPI)
+	fmt.Println(securityTagPayload.Revision)
+	rev := securityTagPayload.Revision
+	newrev,_ := strconv.ParseInt(rev,10,64)
+	newrev +=1
+	securityTagPayload.Revision = strconv.FormatInt(newrev,10)
 	this.BaseAPI = api.NewBaseAPI(http.MethodPut, "/api/2.0/services/securitytags/tag/"+securityTagID, securityTagPayload, new(SecurityTag))
 	return this
 }
--- examples/securitytag.go
+++ examples/securitytag.go
@@ -6,6 +6,7 @@ import (
 	"github.com/sky-uk/gonsx"
 	"github.com/sky-uk/gonsx/api/securitytag"
 	"os"
+
 )
 
 // getAllSecurityTags - gets all securitytags
@@ -97,10 +98,29 @@ func RunSecurityTagExample(nsxManager, nsxUser, nsxPassword string, debug bool) 
 	}
 	getTags, err = getAllSecurityTags(nsxclient)
 	if getTags.CheckByName("test2") {
-		fmt.Println("tag updated")
-	}
-	if getTags.CheckByName("test2") {
 		ID := getTags.FilterByName("test2").ObjectID
+		fmt.Println("Trying to update Tag", ID)
+		updatePayload := securitytag.SecurityTag{
+			ObjectID:    ID,
+			Name:        "test3",
+			Description: "A larger description",
+			TypeName:    "SecurityTag",
+
+		}
+		api := securitytag.NewUpdate(ID, &updatePayload)
+		updateerr := nsxclient.Do(api)
+		if updateerr != nil {
+			fmt.Println("Unable to update tag ", updateerr)
+
+		}
+
+	}
+	if getTags.CheckByName("test3") {
+		fmt.Println("tag updated twice")
+	}
+        return 
+	if getTags.CheckByName("test3") {
+		ID := getTags.FilterByName("test3").ObjectID
 		err := deleteSecurityTag(ID, nsxclient)
 		if err != nil {
 			fmt.Println("Error: ", err)
