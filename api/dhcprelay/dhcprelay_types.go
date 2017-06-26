872a098d8e95665ac7e41c9c8bf60752f125c8c2
rle27 <rui.leitemayermoreira@sky.uk>
2017-06-26 17:01:38 +0100

    the ip address also needs to support a list

--- api/dhcprelay/dhcprelay_types.go
+++ api/dhcprelay/dhcprelay_types.go
@@ -11,7 +11,7 @@ type DhcpRelay struct {
 
 // RelayServer - relayserver within DhcpRelay object.
 type RelayServer struct {
-	IPAddress  string   `xml:"ipAddress"`
+	IPAddress  []string   `xml:"ipAddress"`
 	DomainName []string `xml:"fqdn"`
 }
 
