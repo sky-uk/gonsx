f5b29602ec63f936b4fd445fecff2fc3efadb1e7
sandeep-sidhu <ssa52@ovp.bskyb.com>
2017-06-26 17:40:40 +0100

    Implements Edge resource get and get all.

---
+++ api/edge/get_all_edge.go
@@ -0,0 +1,26 @@
+package edge
+
+import (
+	"github.com/sky-uk/gonsx/api"
+	"net/http"
+	"fmt"
+)
+
+// GetAllEdgesAPI base object.
+type GetAllEdgesAPI struct {
+	*api.BaseAPI
+}
+
+// NewGetAll returns a new object of GetAllEdgesAPI.
+func NewGetAll(pageSize, startIndex int) *GetAllEdgesAPI {
+	url := fmt.Sprintf("/api/4.0/edges?pagesize=%d&startindex=%d", pageSize, startIndex)
+	fmt.Println("URL:", url)
+	this := new(GetAllEdgesAPI)
+	this.BaseAPI = api.NewBaseAPI(http.MethodGet, url, nil, new(PagedEdgeList))
+	return this
+}
+
+// GetResponse returns ResponseObject of PagedEdgeList.
+func (ga GetAllEdgesAPI) GetResponse() PagedEdgeList {
+	return *ga.ResponseObject().(*PagedEdgeList)
+}
