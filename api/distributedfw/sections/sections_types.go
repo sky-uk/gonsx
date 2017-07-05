d9b963f66f21ff8f7a7798fb78ccce2f2ad234c4
rle27 <rui.leitemayermoreira@sky.uk>
2017-07-05 16:34:35 +0100

    fixed lint issues

--- api/distributedfw/sections/sections_types.go
+++ api/distributedfw/sections/sections_types.go
@@ -2,8 +2,9 @@ package sections
 
 import "github.com/sky-uk/gonsx/api/distributedfw/fwrules"
 
+// Section - Contains the rules
 type Section struct {
-	Id    string         `xml:"id,attr"`
+	ID    string         `xml:"id,attr"`
 	Name  string         `xml:"name,attr"`
 	Type  string         `xml:"type,attr"`
 	Rules []fwrules.Rule `xml:"rule"`
