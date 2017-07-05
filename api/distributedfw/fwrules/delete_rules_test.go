3d2606be2d35ea139da8c2a7039a6f9b2d19f1d8
rle27 <rui.leitemayermoreira@sky.uk>
2017-07-05 16:25:20 +0100

    new tests

--- api/distributedfw/fwrules/delete_rules_test.go
+++ api/distributedfw/fwrules/delete_rules_test.go
@@ -9,13 +9,36 @@ import (
 var deleteFWRuleAPI *DeleteFWRuleAPI
 var deletedRule Rule
 
-func setupDeleteRule() {
-	newRule.SectionID = 1234
-	newRule.RuleType = "LAYER3"
+func setupDeleteL3Rule() {
+	deletedRule.SectionID = 1234
+	deletedRule.RuleType = "LAYER3"
+	deleteFWRuleAPI = NewDelete(deletedRule)
+}
+
+func setupDeleteL2Rule() {
+	deletedRule.SectionID = 1234
+	deletedRule.RuleType = "LAYER2"
 	deleteFWRuleAPI = NewDelete(deletedRule)
 }
 
 func TestDeleteMethod(t *testing.T) {
-	setupDeleteRule()
+	setupDeleteL3Rule()
+	assert.Equal(t, http.MethodDelete, deleteFWRuleAPI.Method())
 
+}
+
+func TestL3Endpoint(t *testing.T) {
+	setupDeleteL3Rule()
+	assert.Equal(t, "/api/4.0/firewall/globalroot-0/config/layer3sections", deleteFWRuleAPI.Endpoint())
+}
+
+func TestL2Endpoint(t *testing.T) {
+	setupDeleteL2Rule()
+	assert.Equal(t, "/api/4.0/firewall/globalroot-0/config/layer2sections", deleteFWRuleAPI.Endpoint())
+}
+
+
+func TestL3RuleType(t *testing.T) {
+	setupDeleteL3Rule()
+	assert.Equal(t, "LAYER3", deletedRule.RuleType)
 }
