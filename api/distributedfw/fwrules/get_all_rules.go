fdf3d875553adad1f89ded1f186ca5f93367402d
rle27 <rui.leitemayermoreira@sky.uk>
2017-07-05 17:37:49 +0100

    Fixed format in the edgeinterface demo

--- api/distributedfw/fwrules/get_all_rules.go
+++ api/distributedfw/fwrules/get_all_rules.go
@@ -1,6 +1,7 @@
 package fwrules
 
 import (
+	"fmt"
 	"github.com/sky-uk/gonsx/api"
 	"net/http"
 )
@@ -14,8 +15,13 @@ type GetAllRulesAPI struct {
 func NewGetAll(ruleType, section string) *GetAllRulesAPI {
 	this := new(GetAllRulesAPI)
 	var endpoint string
-	if ruleType == "LAYER3" {
-		endpoint = "/api/4.0/firewall/globalroot-0/config/layer3sections/" + section
+	switch ruleType {
+	case "LAYER3":
+		endpoint = fmt.Sprintf("/api/4.0/firewall/globalroot-0/config/layer3sections/%s", section)
+
+	case "LAYER2":
+		endpoint = fmt.Sprintf("/api/4.0/firewall/globalroot-0/config/layer2sections/%s", section)
+
 	}
 
 	this.BaseAPI = api.NewBaseAPI(http.MethodGet, endpoint, nil, new(Section))
---
+++ api/distributedfw/fwrules/get_all_rules_test.go
@@ -0,0 +1,32 @@
+package fwrules
+
+import (
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"testing"
+)
+
+var getAllRules *GetAllRulesAPI
+
+//var allRules *Section
+
+func setupGetAllRules() {
+	getAllRules = NewGetAll("LAYER3", "1110")
+}
+
+func TestGetAllEndPoint(t *testing.T) {
+	setupGetAllRules()
+	assert.Equal(t, "/api/4.0/firewall/globalroot-0/config/layer3sections/1110", getAllRules.Endpoint())
+}
+
+func TestGetAllMethod(t *testing.T) {
+	setupGetAllRules()
+	assert.Equal(t, http.MethodGet, getAllRules.Method())
+}
+
+func TestGetAllResponse(t *testing.T) {
+	setupGetAllRules()
+	getAllRules.SetResponseObject("test response")
+	assert.Equal(t, "test response", getAllRules.GetResponse())
+
+}
