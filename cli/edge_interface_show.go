0d6ef85113e61da0f4e222e41f729eae2d5afeac
Marco Masetti <marco.masetti@sky.uk>
2017-07-06 18:00:04 +0100

    edge-interface: now support only one interface, all CRUD implemented, tested and provided via cli

--- cli/edge_interface_show.go
+++ cli/edge_interface_show.go
@@ -10,37 +10,40 @@ import (
 	"os"
 )
 
-func getAllEdgeInterfaces(client *gonsx.NSXClient, flagSet *flag.FlagSet) {
+var index int
+
+func getEdgeInterface(client *gonsx.NSXClient, flagSet *flag.FlagSet) {
 
 	if edgeid == "" {
 		fmt.Println("edgeid must be set - usage: -edgeid <a valid edge id>")
 		os.Exit(1)
 	}
 
-	getAllEdgeInterfaceAPI := edgeinterface.NewGetAll(edgeid)
-	err := client.Do(getAllEdgeInterfaceAPI)
+	getEdgeInterfaceAPI := edgeinterface.NewGet(edgeid, index)
+	err := client.Do(getEdgeInterfaceAPI)
 	if err != nil {
 		fmt.Println("error creating edge interfaces: " + err.Error())
 		os.Exit(2)
 	}
 
-	if getAllEdgeInterfaceAPI.StatusCode() == http.StatusOK {
-		interfaces := getAllEdgeInterfaceAPI.GetResponse()
-		fmt.Println("------------ Edge Interfaces ------------")
+	if getEdgeInterfaceAPI.StatusCode() == http.StatusOK {
+		interfaces := getEdgeInterfaceAPI.GetResponse()
+		fmt.Println("------------ Edge Interface ------------")
 		edgesAsXML, err := xml.MarshalIndent(interfaces, "", "  ")
 		if err != nil {
 			panic(err)
 		}
 		fmt.Println(string(edgesAsXML))
 	} else {
-		fmt.Println("Error reading edge interfaces. StatusCode: ", getAllEdgeInterfaceAPI.StatusCode())
+		fmt.Println("Error reading edge interfaces. StatusCode: ", getEdgeInterfaceAPI.StatusCode())
 		os.Exit(3)
 	}
 
 }
 
 func init() {
-	getAllEdgeInterfaceFlags := flag.NewFlagSet("edgeinterface-show", flag.ExitOnError)
-	getAllEdgeInterfaceFlags.StringVar(&edgeid, "edgeid", "", "usage: -edgeid <a valid edge id to attach interfaces to>")
-	RegisterCliCommand("edgeinterface-show", getAllEdgeInterfaceFlags, getAllEdgeInterfaces)
+	getEdgeInterfaceFlags := flag.NewFlagSet("edgeinterface-show", flag.ExitOnError)
+	getEdgeInterfaceFlags.StringVar(&edgeid, "edgeid", "", "usage: -edgeid <a valid edge id to attach interfaces to>")
+	getEdgeInterfaceFlags.IntVar(&index, "index", 0, "usage: -index <the interface index>")
+	RegisterCliCommand("edgeinterface-show", getEdgeInterfaceFlags, getEdgeInterface)
 }
