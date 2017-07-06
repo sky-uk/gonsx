b2006a583565e413afd377c696feb37117909ecf
sandeep-sidhu <ssa52@ovp.bskyb.com>
2017-07-06 16:57:03 +0100

    Fixing golint issues.

--- api/dhcppool/dhcppool_types.go
+++ api/dhcppool/dhcppool_types.go
@@ -1,5 +1,6 @@
 package dhcppool
 
+// DHCPPool struct
 type DHCPPool struct {
 	IPRange             string `xml:"ipRange"`
 	DefaultGateway      string `xml:"defaultGateway"`
@@ -12,16 +13,19 @@ type DHCPPool struct {
 	NextServer          string `xml:"nextServer"`
 }
 
+// DHCPOptions struct
 type DHCPOptions struct {
 	Option121 option121 `xml:"option121"`
 	Option66  string    `xml:"option66"`
 	option67  string    `xml:"option67"`
 }
 
+// option121 struct
 type option121 struct {
 	StaticRoute staticRoute `xml:"option121>staticRoute"`
 }
 
+// staticRoute struct
 type staticRoute struct {
 	DestinationSubnet string `xml:"destinationSubnet"`
 	Router            string `xml:"router"`
