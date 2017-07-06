d777f9b277d8e3242ef5ff031ca13ba57a012045
sandeep-sidhu <ssa52@ovp.bskyb.com>
2017-07-06 15:47:33 +0100

    adding DHCP pool types

--- api/dhcppool/dhcppool_types.go
+++ api/dhcppool/dhcppool_types.go
@@ -1,27 +1,22 @@
 package dhcppool
 
-
 type DHCPPool struct {
-
-	IPRange string `xml:"ipRange"`
-	DefaultGateway string `xml:"defaultGateway"`
-	SubnetMask string `xml:subnetMask`
-	DomainName string `xml:"domainName"`
-	PrimaryNameServer string `xml:"primaryNameServer"`
+	IPRange             string `xml:"ipRange"`
+	DefaultGateway      string `xml:"defaultGateway"`
+	SubnetMask          string `xml:subnetMask`
+	DomainName          string `xml:"domainName"`
+	PrimaryNameServer   string `xml:"primaryNameServer"`
 	SecondaryNameServer string `xml:"secondaryNameServer"`
-	LeaseTime uint8 `xml:"leaseTime"`
-	AutoConfigDNS bool `xml:"autoConfigureDNS"`
-	NextServer string `xml:"nextServer"`
-
+	LeaseTime           uint8  `xml:"leaseTime"`
+	AutoConfigDNS       bool   `xml:"autoConfigureDNS"`
+	NextServer          string `xml:"nextServer"`
 }
 
 type DHCPOptions struct {
 	Option121 option121 `xml:"option121"`
-	Option66 string `xml:"option66"`
-	option67 string `xml:"option67"`
-
+	Option66  string    `xml:"option66"`
+	option67  string    `xml:"option67"`
 }
-
 
 type option121 struct {
 	StaticRoute staticRoute `xml:"option121>staticRoute"`
@@ -29,5 +24,5 @@ type option121 struct {
 
 type staticRoute struct {
 	DestinationSubnet string `xml:"destinationSubnet"`
-	Router string `xml:"router"`
+	Router            string `xml:"router"`
 }
