fbf3b53b8818b6a071da2bf09a64ac75ade993ca
rle27 <rui.leitemayermoreira@sky.uk>
2017-07-11 16:05:46 +0100

    initial code for creating and deleting sections

--- api/distributedfw/sections/create_section.go
+++ api/distributedfw/sections/create_section.go
@@ -1,1 +1,31 @@
 package sections
+
+import (
+	"fmt"
+	"github.com/sky-uk/gonsx/api"
+	"net/http"
+)
+
+// CreateFWRulesAPI default struct
+type CreateFWSectionsAPI struct {
+	*api.BaseAPI
+}
+
+func NewCreate(newSection Section) *CreateFWSectionsAPI {
+	this := new(CreateFWSectionsAPI)
+	var endpoint string
+	switch newSection.Type {
+	case "LAYER3":
+		endpoint = fmt.Sprintf("/api/4.0/firewall/globalroot-0/config/layer3sections/%d", newSection.ID)
+	case "LAYER2":
+		endpoint = fmt.Sprintf("/api/4.0/firewall/globalroot-0/config/layer2sections/%d", newSection.ID)
+	}
+	this.BaseAPI = api.NewBaseAPI(http.MethodPost, endpoint, newSection, new(Section))
+	return this
+
+}
+
+// GetResponse - Returns ResponseObject from GetAllFirewallRulesAPI of Rule type.
+func (createAPI CreateFWSectionsAPI) GetResponse() Section {
+	return *createAPI.ResponseObject().(*Section)
+}
