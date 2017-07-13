c33a05de4d76762d910e903987dda3cd46ab8881
rle27 <rui.leitemayermoreira@sky.uk>
2017-07-13 09:46:53 +0100

    added update for section

---
+++ api/distributedfw/sections/update_section.go
@@ -0,0 +1,32 @@
+package sections
+
+import (
+	"fmt"
+	"github.com/sky-uk/gonsx/api"
+	"net/http"
+)
+
+// CreateFWSectionsAPI default struct
+type UpdateFWSectionsAPI struct {
+	*api.BaseAPI
+}
+
+// NewCreate - Creates a new section
+func NewUpdate(updateSection Section) *UpdateFWSectionsAPI {
+	this := new(UpdateFWSectionsAPI)
+	var endpoint string
+	switch updateSection.Type {
+	case "LAYER3":
+		endpoint = fmt.Sprintf("/api/4.0/firewall/globalroot-0/config/layer3sections/%s", updateSection.ID )
+	case "LAYER2":
+		endpoint = fmt.Sprintf("/api/4.0/firewall/globalroot-0/config/layer2sections/%s", updateSection.ID)
+	}
+	this.BaseAPI = api.NewBaseAPI(http.MethodPost, endpoint, updateSection, new(Section))
+	return this
+
+}
+
+// GetResponse - Returns ResponseObject from GetAllFirewallRulesAPI of Rule type.
+func (updateAPI UpdateFWSectionsAPI) GetResponse() Section {
+	return *updateAPI.ResponseObject().(*Section)
+}
